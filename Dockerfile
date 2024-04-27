3b0cd69b313b7a5b7ea71235855547c397dca38a
Naastyya <nastya.slota31@gmail.com>
2024-04-27 13:57:13 +0300

    INIT

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node:16
 WORKDIR /usr/src/app
-COPY package*.json app.js ./
+COPY package*.json ./
 RUN npm install
+COPY app.js ./
 EXPOSE 80
-CMD ["node","app.js"]
+CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 80; // Use environment variable for port or default to 80
+
 app.get('/', (req, res) => res.send('Hello Express'))
-app.listen(80)
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
