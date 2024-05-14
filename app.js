e0addbac6b9239e01defc23c2483f0fa3ae41a05
Naastyya <nastya.slota31@gmail.com>
2024-05-14 14:44:18 +0300

    changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 80;
 
 // Встановлюємо шлях до папки, в якій містяться статичні файли (стилі, зображення і т.д.)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
