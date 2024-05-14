5b600e414c6bbb5ef948d38a22effcb92e4e62a6
Naastyya <nastya.slota31@gmail.com>
2024-05-14 14:33:12 +0300

    changes

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 const express = require('express');
-const fs = require('fs'); // модуль для роботи з файловою системою
-const path = require('path'); // модуль для роботи з шляхами файлів
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 80;
 
+// Встановлюємо шлях до папки, в якій містяться статичні файли (стилі, зображення і т.д.)
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    // Вчитуємо вміст файлу index.html
-    fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            // Якщо сталася помилка, відправте клієнту відповідь з кодом статусу 500
-            res.status(500).send('Internal Server Error');
-            return;
-        }
-        // Відправляємо вміст файлу index.html як відповідь на запит
-        res.send(data);
-    });
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
